fbebad62a5494ae7322df80a3c1792f0b5950203
nazrulcse <nazrulku07@gmail.com>
2018-02-07 11:05:15 +0600

    chosen image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ end
 Rails.application.config.middleware.use ExceptionNotification::Rack,
                                         :email => {
                                             :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
-                                            :email_prefix => "[Bequent] ",
-                                            :sender_address => %{"notifier" <notifier@example.com>},
-                                            :exception_recipients => %w{pias221191@gmail.com gmarifulislamarif@gmail.com}
+                                            :email_prefix => "[EPOS] ",
+                                            :sender_address => %{"notifier" <notifier@epos.com>},
+                                            :exception_recipients => %w{pias221191@gmail.com}
                                         }
