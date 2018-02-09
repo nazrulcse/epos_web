2b5f3c10c7174552839c26b70f6a2f486951d29e
nazrulcse <nazrulku07@gmail.com>
2018-02-09 19:24:47 +0600

    product index page info changes and stock page search worked

--- app/controllers/pos/stocks_controller.rb
+++ app/controllers/pos/stocks_controller.rb
@@ -1,7 +1,7 @@
 class Pos::StocksController < ApplicationController
 
   def index
-    @products = current_department.products
+    @products = current_department.products.search(params[:q])
   end
 
   def history
